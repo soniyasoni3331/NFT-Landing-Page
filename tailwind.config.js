cf3a06b8bdc8cfe3ccc9d99e5e198855ae5eb7fc
soniyasoni3331 <soniyasoni3331@gmail.com>
2023-08-03 01:24:55 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      abc: ["Gruppo", "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
